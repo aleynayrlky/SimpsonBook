bb30278b037d81c1e0a17814a54c433ac53846c0
Aleyna Yerlikaya <aleynayrlky@gmail.com>
2024-03-02 15:14:00 +0300

    simpsonbook added

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,21 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Aleyna Yerlikaya on 2.03.2024.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage){
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+}
